f9ac99f594e9bb7f88a3c17f9af93f0f4d5ee418
ArtsiomChults <artemorsha@mail.ru>
2025-11-11 23:10:30 +0300

    fix: import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { PrismaModule } from "../prisma/prisma.module"
 import { AuthModule } from "./auth/auth.module"
+import { HandbookModule } from "./handbook/handbook.module"
 import { UsersModule } from "./users/users.module"
-import { HandbookModule } from './handbook/handbook.module';
 
 @Module({
 	imports: [

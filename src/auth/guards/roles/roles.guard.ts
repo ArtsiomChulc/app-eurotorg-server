618ec2ccdb88f44ca20bcf7be36d7e664fda9209
ArtsiomChults <artemorsha@mail.ru>
2025-11-18 22:17:07 +0300

    fix: any type

--- src/auth/guards/roles/roles.guard.ts
+++ src/auth/guards/roles/roles.guard.ts
@@ -1,20 +1,26 @@
 import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common"
 import { Reflector } from "@nestjs/core"
 import { ROLES_KEY } from "../../../utils/decorators/roles.decorator"
+import { Request } from "express"
+
+interface RequestWithUser extends Request {
+	user: {
+		role: string
+	}
+}
 
 @Injectable()
 export class RolesGuard implements CanActivate {
 	constructor(private reflector: Reflector) {}
 
 	canActivate(context: ExecutionContext): boolean {
+		const request = context.switchToHttp().getRequest<RequestWithUser>()
 		const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
 			context.getHandler(),
 			context.getClass()
 		])
 
 		if (!requiredRoles) return true
-
-		const { user } = context.switchToHttp().getRequest()
-		return requiredRoles.includes(user.role)
+		return requiredRoles.includes(request.user.role)
 	}
 }

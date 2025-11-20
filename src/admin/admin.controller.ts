4c1bf4a8f21cc94097173f9dedb88b40665d07c6
ArtsiomChults <artemorsha@mail.ru>
2025-11-20 14:55:09 +0300

    add: crete director

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseGuards } from "@nestjs/common"
+import { Controller, Get, UseGuards, Body, Post } from "@nestjs/common"
 import { JwtAccessGuard } from "../auth/guards/jwt-access.guard"
 import { RolesGuard } from "../auth/guards/roles/roles.guard"
+import { CreatePersonDto } from "../person/dto/create-person.dto"
 import { Roles } from "../utils/decorators/roles.decorator"
 import { AdminService } from "./admin.service"
 
@@ -25,5 +26,10 @@ export class AdminController {
 	@Roles("ADMIN")
 	findAll() {
 		return this.adminService.findAllEngineers()
+	}
+
+	@Post("director")
+	createDirector(@Body() dto: CreatePersonDto) {
+		return this.adminService.createDirector(dto)
 	}
 }
